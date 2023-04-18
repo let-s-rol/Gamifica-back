a26009a3f8ccba1984d43d9c5c16998c6f18c71a
BryanCayetano <ilerna.bryan@gmail.com>
2023-04-18 17:57:58 +0200

    s

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ Route::group(['middleware' => ["auth:sanctum"]], function () {
     Route::post('update_points', [Ranking_UserController::class, 'update_points']);
     Route::get('show_students', [Ranking_UserController::class, 'show_students']);
     Route::get('show_pending_users', [Ranking_UserController::class, 'show_pending_users']);
-    Route::put('validate_student', [Ranking_UserController::class, 'validate_user']);
+    Route::post('validate_student', [Ranking_UserController::class, 'validate_user']);
     Route::delete('kick_student', [Ranking_UserController::class, 'kickoff']);
     //TASKS
     Route::post('tasks/{id_task}/insert', [Task_userController::class, 'insertTask']);
